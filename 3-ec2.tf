4088a98df8decde6f7b8ee5bc9c743e12c136435
Abdelmajid El-oualy <abdel-ou@c1r8p1.1337.ma>
2025-09-01 12:59:00 +0100

    deploy only one

--- 3-ec2.tf
+++ 3-ec2.tf
@@ -5,32 +5,11 @@ resource "aws_instance" "web_server" {
   key_name      = "1337"
   vpc_security_group_ids = [aws_security_group.Inception_aws_security_group.id]
 
+  # Load the script from lol.sh and pass it as user_data
+  user_data = file("/Users/abdel-ou/Desktop/cloud-1/lol.sh") # Replace "path/to/lol.sh" with the actual path to your script
+
   tags = {
     Name = "web-server"
-  }
-}
-
-resource "aws_instance" "data_base" {
-  count         = 1
-  ami           = "ami-0360c520857e3138f" 
-  instance_type = "t2.micro"
-  key_name      = "1337"
-  vpc_security_group_ids = [aws_security_group.Inception_aws_security_group.id]
-
-  tags = {
-    Name = "data-bases"
-  }
-}
-
-resource "aws_instance" "wordpress" {
-  count         = 1
-  ami           = "ami-0360c520857e3138f" 
-  instance_type = "t2.micro"
-  key_name      = "1337"
-  vpc_security_group_ids = [aws_security_group.Inception_aws_security_group.id]
-
-  tags = {
-    Name = "wordpress"
   }
 }
 
